3255b6052ce5ab171e58213c2128bd76b36deee2
Erizium <tobias.osterlin@gmail.com>
2021-09-03 12:36:22 +0200

    Fixed dog api

--- components/FetchDog.js
+++ components/FetchDog.js
@@ -12,26 +12,35 @@ const FetchDog = () => {
     const url = "https://api.jsonbin.io/b/6130c06e2f1cab416bd26653";
     const [isLoading, setLoading] = useState(true);
     const [data, setData] = useState(null);
+    const [dogFact, setDogFact] = useState(null)
+    const [counter, setCounter] = useState(0);
 
-    const {dogValue, setDogValue} = useContext(FavoriteDogFact);
-    
-    function getDogFact() {
+    const { dogValue, setDogValue } = useContext(FavoriteDogFact);
+
+    useEffect(() => {
         fetch(url)
             .then((response) => response.json())
-            .then((json) => {
-                setData(json.facts)
-            })
+            .then((json) => setData(json.facts))
             .catch((error) => alert(error))
-            .finally(setLoading(false))
+
+    }, [])
+
+    function getDogFact() {
+        console.log(data[counter])
+        setLoading(false)
+        if(counter >= data.length - 1) {
+            setCounter(0)
+        }
     };
-    
+
 
     return (
-        <SafeAreaView style={{ alignItems: "center" }}>   
+        <SafeAreaView style={{ alignItems: "center" }}>
             <View style={{ marginTop: "50px" }}>
                 <Button
                     title="Fact Button!"
                     onPress={() => {
+                        setCounter(counter + 1)
                         //hämtar hund fakta
                         getDogFact();
                     }}>
@@ -45,20 +54,22 @@ const FetchDog = () => {
                         margin: "20px", marginTop: "10px", padding: "5px",
                         borderRadius: "5px", border: "1px solid black", background: "lightgray"
                     }}
-                    >{data}</Text>
+                    >{data[counter]}</Text>
 
                     <View style={{ display: "flex", flexDirection: "column", alignSelf: "flex-end" }}>
                         <TouchableOpacity
                             activeOpacity={0.4}
                             onPress={() => {
                                 //lägg till favoritfakta längre ner 
-                                setDogValue(data)
+                                setDogValue(data[counter])
                             }}>
                             <Image source={{
                                 uri: "https://www.pinclipart.com/picdir/middle/44-448748_star-wand-cliparts-smiling-stars-clipart-png-download.png"
                             }}
-                                style={{ width: "50px", height: "50px", alignSelf: "flex-end", 
-                                marginRight: "20px", marginTop: "-10px", alignSelf: "center" }}
+                                style={{
+                                    width: "50px", height: "50px", alignSelf: "flex-end",
+                                    marginRight: "20px", marginTop: "-10px", alignSelf: "center"
+                                }}
                             />
                         </TouchableOpacity>
                         <Text style={{ marginRight: "20px" }}>Favorite</Text>
